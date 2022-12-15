bc22871a22fe436e5c0d26cdd1c02197f1d14037
FarajMonnapillai <farajahmed2001@gmail.com>
2022-12-15 16:59:18 +0000

    Creating room complete

--- SocialVR/Assets/Scripts/Photon2/TestConnect.cs
+++ SocialVR/Assets/Scripts/Photon2/TestConnect.cs
@@ -6,9 +6,10 @@ using UnityEngine;
 
 public class TestConnect : MonoBehaviourPunCallbacks
 {
+
     private void Start()
     {
-        print("Connecting to server.");
+        Debug.Log("Connecting to server.", this);
         PhotonNetwork.NickName = MasterManager.GameSettings.Nickname;
         PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
         PhotonNetwork.ConnectUsingSettings();
@@ -17,20 +18,18 @@ public class TestConnect : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         Debug.Log("Connected to server.", this);
+        print(PhotonNetwork.LocalPlayer.NickName);
 
-        Debug.Log("My Nickname is " + PhotonNetwork.LocalPlayer.NickName, this);
-        if(!PhotonNetwork.InLobby)
             PhotonNetwork.JoinLobby();
     }
 
     public override void OnDisconnected(DisconnectCause cause)
     {
-        Debug.Log("Failed to connect to Photon: " + cause.ToString(), this);
+        Debug.Log("Failed to connect to Server: " + cause.ToString(), this);
     }
 
     public override void OnJoinedLobby()
     {
         print("Joined lobby");
     }
-
 }
