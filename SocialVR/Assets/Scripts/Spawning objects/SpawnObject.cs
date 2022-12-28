27a3250cb81ffd5be880372afcb9ee1261f09efb
FarajMonnapillai <farajahmed2001@gmail.com>
2022-12-28 18:00:45 +0000

    Objects assigiment

---
+++ SocialVR/Assets/Scripts/Spawning objects/SpawnObject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SpawnObject : MonoBehaviour
+{
+    public GameObject MycubrPrefab;
+
+    public void InstantiateCaller(GameObject prefab)
+    {
+        Instantiate(prefab);
+        prefab.transform.localPosition = new Vector3(0,1,2);
+    }
+}
+
+
